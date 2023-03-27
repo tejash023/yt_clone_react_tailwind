2fb0c0f4e7e65b213894a0d19d87ae6b6f89d682
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-03-27 23:26:45 +0530

    completed header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,42 @@
 import React from "react";
+import { FiSearch } from "react-icons/fi";
 
 const Header = () => {
-  return <div>Head</div>;
+  return (
+    <div className="grid grid-flow-col p-0.5  m-0.5 px-4 shadow-lg items-center">
+      <div className="flex items-center col-span-1">
+        <img
+          className="h-6"
+          src="https://cdn.iconscout.com/icon/free/png-256/hamburger-menu-462145.png"
+          alt="nav-icon"
+        />
+
+        <img
+          className="h-14 mx-2"
+          src="https://logos-world.net/wp-content/uploads/2020/04/YouTube-Logo.png"
+          alt="logo"
+        />
+      </div>
+
+      <div className="flex col-span-10 px-10 h-8 ">
+        <input
+          className="w-1/2 border border-gray-400 p-2 rounded-l-full focus:outline-none  "
+          type="text"
+          placeholder="Search videos.."
+        />
+        <button className=" border border-gray-400 px-5 bg-gray-100 p-2 rounded-r-full">
+          <FiSearch height="1.15rem" />
+        </button>
+      </div>
+
+      <div className="col-span-1">
+        <img
+          className="h-8"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+          alt="user-icon"
+        />
+      </div>
+    </div>
+  );
 };
 export default Header;
