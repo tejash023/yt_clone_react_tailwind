a74b616ea936c475db129a34b8e52fac04c1a985
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-04-06 02:14:08 +0530

    added like dislike and subscribe

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -3,7 +3,12 @@ import { useSearchParams } from "react-router-dom";
 import ReactPlayer from "react-player";
 import { fetchFromAPI } from "../utils/constant";
 
-import { MdThumbUpOffAlt, MdThumbDownOffAlt } from "react-icons/md";
+import {
+  MdThumbUpOffAlt,
+  MdThumbDownOffAlt,
+  MdThumbUp,
+  MdThumbDown,
+} from "react-icons/md";
 
 const WatchPage = () => {
   const [searchParams] = useSearchParams();
@@ -11,6 +16,9 @@ const WatchPage = () => {
   const id = searchParams.get("v");
 
   const [videoDetail, setVideoDetail] = useState(null);
+  const [subscribed, setSubscribed] = useState(false);
+  const [isLiked, setIsLiked] = useState(false);
+  const [isDisliked, setIsDisliked] = useState(false);
 
   useEffect(() => {
     const getVideoDetails = async () => {
@@ -41,17 +49,40 @@ const WatchPage = () => {
             <p className="font-bold">{videoDetail.snippet.channelTitle}</p>
             <div className="flex items-center justify-between gap-1 py-2">
               <div className="flex items-center mr-4">
-                <button className="bg-red-600 text-white rounded-l-full py-2 px-5 ">
-                  <MdThumbUpOffAlt size="1.5rem" />
+                <button
+                  onClick={() => {
+                    setIsLiked(!isLiked);
+                    setIsDisliked(false);
+                  }}
+                  className="bg-red-600 text-white rounded-l-full py-2 px-5 "
+                >
+                  {isLiked ? (
+                    <MdThumbUp size="1.5rem" />
+                  ) : (
+                    <MdThumbUpOffAlt size="1.5rem" />
+                  )}
                 </button>
 
-                <button className="bg-red-600 text-white rounded-r-full py-2 px-5">
-                  <MdThumbDownOffAlt size="1.5rem" />
+                <button
+                  onClick={() => {
+                    setIsDisliked(!isDisliked);
+                    setIsLiked(false);
+                  }}
+                  className="bg-red-600 text-white rounded-r-full py-2 px-5"
+                >
+                  {isDisliked ? (
+                    <MdThumbDown size="1.5rem" />
+                  ) : (
+                    <MdThumbDownOffAlt size="1.5rem" />
+                  )}
                 </button>
               </div>
 
-              <button className="bg-slate-700 text-white rounded-full py-2 px-5">
-                Subscibe
+              <button
+                onClick={() => setSubscribed(!subscribed)}
+                className="bg-slate-700 text-white rounded-full py-2 px-5 cursor-pointer"
+              >
+                {subscribed ? "Subscribed" : "Subscibe"}
               </button>
             </div>
           </div>
